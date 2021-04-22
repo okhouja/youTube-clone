b740afbc6836de5ce68382391454c969a68a792a
Omar <omar0940@gmail.com>
2021-04-22 16:04:56 +0200

    dark mode and light mode

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const theme = () => {
   const mood = document.querySelector("#light-dark").checked;
+  const video = document.querySelector(".videos-container").style;
   body = document.querySelector("body").style;
   console.log(mood);
   if (mood) {
     body.backgroundColor = "black";
+    video.backgroundColor = "blue";
     body.fonts = "white";
   } else {
     body.backgroundColor = "white";
+    video.backgroundColor = "grey";
+
     body.fonts = "black";
   }
 };
